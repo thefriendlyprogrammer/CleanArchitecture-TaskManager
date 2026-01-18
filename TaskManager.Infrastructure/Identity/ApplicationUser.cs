ac99a1738c104436824b2b46458182c6426ba291
Alok Kushwaha <alok.info90@gmail.com>
2026-01-18 23:11:00 +0530

    Added identity folder and identity custom class

---
+++ TaskManager.Infrastructure/Identity/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TaskManager.Infrastructure.Identity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; } = string.Empty;
+    }
+}
